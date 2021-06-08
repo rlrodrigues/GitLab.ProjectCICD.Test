2c3d66234e5b5d87717ba0bd3f13f033980d739a
Ruan Rodrigues <rrodri@outlook.com.br>
2021-06-08 18:58:37 -0300

    Add project files.

---
+++ GitLab.ProjectCICD.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitLab.ProjectCICD.Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
